21f54e4e5a393f7c746483e0e3312b56c65b56da
manuraghaw123 <31969055+manuraghaw123@users.noreply.github.com>
2024-10-26 17:32:32 +0530

    till now

---
+++ SupremeCourt_Exhibit28/Assets/_Main/Manager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private Scrollbar scrollBar;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Image filler;
+    public void OnSliderValueChange()
+    {
+        scrollBar.value = slider.value;
+        filler.fillAmount = Remap(slider.value, 0, 1, 1, 0);
+       
+    }
+
+    public void OnScrollBarValueChange()
+    {
+        slider.value = scrollBar.value;
+      
+    }
+
+    private float Remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+
+
+
+
+}
